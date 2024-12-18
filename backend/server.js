fc51d48208931525adfdc7f66ceea92357d6c2a3
Julius <jkpodo05@gmail.com>
2024-12-18 19:41:40 +0000

    More modifications to api location

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Updated CORS configuration
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500'],
+    origin: "*",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://campuseventmanagement-backend.onrender.com',
         changeOrigin: true
       }
     }
